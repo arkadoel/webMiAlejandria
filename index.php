8216974efe2dc724ef6bece891167b380d585803
Fer D Minguela <fer.d.minguela@gmail.com>
2014-04-03 20:15:43 +0200

    Primer guardado Abril 2014

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<!--
+Author: Fernando D. Minguela
+Fecha de creacion: 27-Dic-2013
+Actualizacion: 25-Mar-2014
+-->
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <title>Mi Alejandria, Big Data, procesamiento de datos</title>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+        <link href='http://fonts.googleapis.com/css?family=PT+Mono|Fugaz+One|Imprima' rel='stylesheet' type='text/css'>
+        <link rel="shortcut icon" href="imgs/favicon.ico" type="image/png" />
+        
+        <meta name="description" content="Big Data, Procesamiento de datos y documentos - Mi Alejandria">
+        <meta name="keywords" content="Big data, Big data spain, Big data Castilla y Leon, documentos,documentación,analisis, procesamiento de información, estadisticas, almacenamiento"> 
+        <meta name="robots" content="index,follow,all,noarchive,noodp">
+        <meta http-equiv="content-language" content="es">
+        <meta http-equiv="cache-control" content="no-cache" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta http-equiv="expires" content="0" />
+        
+        
+          <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        
+        <script type="text/javascript" >
+        $(function() {                
+            $('a[href*=#]:not([href=#])').click(function() {
+              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  $('html,body').animate({
+                    scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+                }
+              }
+            });
+            
+            
+            
+          });
+        </script>
+    </head>
+    <body>
+        <div class="cajaSuperior">
+            <a href="#Inicio">
+            <div class="cajaMenu" >
+                <img src="imgs/LogoMiAlejandria.png" style="vertical-align: middle; width: 72px; height: 72px;" />
+                <h1>Mi Alejandría</h1>
+            </div>
+            </a>
+            <div class="cajaMenu2" style="width: 30px;" >                
+            </div>
+           
+            <div class="cajaMenu2" >
+                <br/> <a href="#Contacto" >Contacto </a>
+            </div>           
+            <div class="cajaMenu2" >
+                <br/> <a href="http://mialejandria.blogspot.com.es">Blog</a>
+            </div>
+             <div class="cajaMenu2" >
+                <br/> <a href="#somos">Quienes <br/>somos</a>
+            </div>
+             <div class="cajaMenu2" >
+                <br/> <a href="#ofrecemos">Servicios</a>
+            </div>
+           
+        </div>
+        <br/>
+        <div class="apartado" style="padding: 0px;" id="Inicio" >
+            <br/><br/><br/>
+            
+            <img src="imgs/Iceberg.jpg" class="img_inicio" />
+            <br/><br/><br/><br/><br/><br/>
+            <div class="div_inicio">
+                La información que usa para sus decisiones sólo es la punta del iceberg
+                de todo lo que su empresa contiene.
+            </div>
+            
+            
+        </div>        
+        <div class="apartado" id="ofrecemos" style="/*background-color: #c2e1c2;*/background-color: #cccccc;">
+            <br/><br/><br/><br/>
+            <h2>¿Qué hacemos?</h2>
+            <div class="tabla">
+                <div class="div_caja">
+                    <h3 style="text-align: center">Procesamiento express de información</h3>
+                    <img src="imgs/procesamiento.png" class="img_para_cajas" /><br/>
+                    Si necesita generar una gran cantidad de documentación en un formato que debe entregar y no hay 
+                    tiempo humano suficiente para hacerlo, nosotros podemos procesar su información y generar los documentos que 
+                    necesite mediante procesamiento informático.
+                    </div>
+                <div class="div_caja" style="width: 700px; text-align: center;">
+                    <h3 style="text-align: center">Analisis estadistico</h3>
+                    <img src="imgs/chart.png" class="img_para_cajas"  /><br/><br/>
+                    <span style="text-align: left;">
+                    Servicios de analisis para:
+                    <ul>
+                        <li>FeedBack y Logs:</li>
+                        <li>Control de versiones y testing</li>
+                        <li>SharePoint</li>
+                    </ul>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="apartado" id="somos" style="background-color: #999999; /*#8fbbc5;*/" >
+            <br/><br/><br/><br/>
+            <h2>¿Quienes somos?</h2>
+            Nos dedicamos a interpretar información, relacionarla y generar nuevos datos de mayor valor 
+estrategico para que nuestros clientes puedan disponer de mejor información para tomar sus decisiones y puedan basarse en datos reales.
+            <br/>
+            <hr>
+            <div class="tabla">
+                <div class="caja">
+                    <h3>Nuestra Misión</h3>
+                    <!--finalidad o razon de ser del negocio ¿por qué existimos? 
+                    ¿Que justifica nuestra existencia ? -->
+                    La gran cantidad de infomación que circula por las empresas, la cual no suele estar toda estructurada en bases de datos que 
+se usen, sino que suele estar contenida en multitud de archivos, documentos, etc...
+                </div>
+                <div class="caja">
+                    <h3>Nuestra Visión</h3>
+                    <!-- ¿como deseamos ser reconocidos por nuestros clientes? 
+                    ¿Como queremos que se sientan nuestros profesionales en nuestra organizacion?
+                    -->
+                    Seremos las personas que consigan extraer informacion valiosa para 
+                    su empresa, recopilando aquella informacion que antes no le servía y 
+                    transformandola en datos estrategicos.
+                </div>
+                <div class="caja">
+                    <h3>Nuestros valores</h3>
+                    <ul>
+                        <li>Transparencia</li>
+                        <li>Inteligencia</li>
+                        <li>Creatividad</li>
+                        <li>Adaptabilidad</li>
+                        <li>Proactividad</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+        <div class="apartado" id="Contacto" style="color:white; background-color: #414141;">
+            <br/><br/><br/><br/>
+            <h2 style="color: #919191">Contacta con nosotros</h2>
+            
+            <h3 style="font-size: 20pt; margin-left: 10px;">info @ mialejandria.es</h3>
+            <br/>
+            <h3 style="color: #919191; margin-left: 10px;">O siguenos en tu red social favorita</h3>
+            <!-- FACEBOOOK -->
+            <a href="https://www.facebook.com/mi.alejandria.9">
+                <img src="imgs/Facebook-icon.png" class="img_social" />
+            </a>
+            
+            <a href="https://github.com/MiAlejandria">
+            <img src="imgs/GitHub-icon.png" class="img_social" />
+            </a>
+            
+            <!-- GOOGLE PLUS -->
+            <a href="//plus.google.com/u/0/107935481520192512926?prsrc=3" rel="publisher" target="_top" style="text-decoration:none;">
+                <img src="imgs/GooglePlus-icon.png" class="img_social" />
+            </a>
+            
+            <!--Linkedin -->
+            <a href="javascript:void(window.open('http://www.linkedin.com/shareArticle?url=http://mialejandria.es','','width=600,height=400,left=50,top=50'));">
+                <img src="imgs/LinkedIn-icon.png" class="img_social" />
+            </a>
+            
+            <!-- youtube -->
+            <a href="https://www.youtube.com/channel/UCthiDuVOQcw9fUmHiyzMpNA">
+                <img src="imgs/Youtube-icon.png" class="img_social" />
+            </a>
+            
+            <a href="https://twitter.com/MiAlejandria" >
+            <img src="imgs/Twitter-icon.png" class="img_social" />
+            </a>
+            
+        </div>
+       
+        <!-- GOOGLE ANALYTICS -->
+        <script> 
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-49310054-1', 'mialejandria.es');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
